f1429f0c0a429574a5c4cf56965f706c1dab06f0
tarang-gaurav <37691618+tarang-gaurav@users.noreply.github.com>
2020-04-09 13:41:39 +0530

    Update Solution.java

--- Data Structures/LinkedList/PrintLinkedList/Solution.java
+++ Data Structures/LinkedList/PrintLinkedList/Solution.java
@@ -1,1 +1,24 @@
 
+
+
+    // Complete the printLinkedList function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void printLinkedList(SinglyLinkedListNode head) {
+        SinglyLinkedListNode node= head;
+        while(node!=null)
+        {
+            System.out.println(node.data + " "); 
+            node = node.next; 
+        } 
+
+    }
+
