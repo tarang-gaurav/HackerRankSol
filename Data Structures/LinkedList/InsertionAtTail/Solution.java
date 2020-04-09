5a72f26241f379891f0abdec49294bfdba117812
tarang-gaurav <37691618+tarang-gaurav@users.noreply.github.com>
2020-04-09 13:42:40 +0530

    Update Solution.java

--- Data Structures/LinkedList/InsertionAtTail/Solution.java
+++ Data Structures/LinkedList/InsertionAtTail/Solution.java
@@ -1,1 +1,38 @@
 
+
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+
+        SinglyLinkedListNode newNode=new SinglyLinkedListNode(data);
+       // newNode.data=data;
+        newNode.next=null;
+
+        if(head==null){
+            head=newNode;
+           return head;
+           
+        }
+       // newNode.next=null;
+        
+            SinglyLinkedListNode curr=head;
+            while(curr.next!=null){
+                curr=curr.next;
+            }
+              curr.next=newNode;  
+            
+             
+            return head;
+        
+    }
+
